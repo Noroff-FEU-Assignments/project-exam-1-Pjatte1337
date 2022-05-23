66bd05fb2f07cfab6f23b975aa369e9a0a6ca72b
Pjatte <91551936+Pjatte1337@users.noreply.github.com>
2022-05-23 20:11:32 +0200

    modal

--- js/blog-specific.js
+++ js/blog-specific.js
@@ -1,5 +1,5 @@
 const contentContainer = document.querySelector(".container");
-const modalz = document.querySelector(".modalz");
+
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
@@ -11,87 +11,117 @@ console.log(id);
 newTitle.innerHTML = "";
 
 const url = `https://www.joakimvanebo.one/wp-json/wp/v2/blogs2/${id}?acf_format=standard`;
+
 console.log(url);
 
 
+
 async function fetchContent() {
+
     try {
         const response = await fetch(url);
-        const content = await response.json();
+        const post = await response.json();
 
-        console.log(content);
+        console.log(post);
 
-        createHTML(content);
+            const headline = post.acf.h1;
+            const img1 = post.acf.image1;
+            const title = post.acf.h2;
+            const paragraph1 = post.acf.p1;
+            const paragraph2 = post.acf.p2;
+            const img2 = post.acf.image2;
+            const img3 = post.acf.image3;
+            const img4 = post.acf.image4;
+            const paragraph3 = post.acf.p3;
+            const paragraph4 = post.acf.p4;
+            const img5 = post.acf.image5;
+            const paragraph5 = post.acf.p5;
+            const paragraph6 = post.acf.p6;
+            const score = post.acf.score;
+            const lineup1 = post.acf.lineup1;
+            const lineup2 = post.acf.lineup2;
+            const HTMLTitle = post.acf.name;
+            const author = post.acf.author;
 
-    }catch(error){
-    }
+            
 
+            contentContainer.innerHTML = `  <div class="blog-content">
+                                            <h1 class="headline">${headline}</h1>
+
+                                            <div class="top">
+                                            <img src="${img1}" class="image image-one" alt="${headline}"/>
+                                            </div>
+
+                                            <div class="content-one"> 
+                                            <h2 class="headline-two">${title}</h2>
+                                            <p class="paragraph-one">${paragraph1}</p>
+                                            <p class="paragraph-two">${paragraph2}</p>
+                                            </div>
+
+                                            <div class="content-two"> 
+                                            <img src="${img2}" class="image image-two" alt="${headline}"/>
+                                            <img src="${img3}" class="image image-three" alt="${headline}"/>
+                                            <img src="${img4}" class="image image-four" alt="${headline}"/>
+                                            </div>
+
+                                            <div class="content-three"> 
+                                            <p class="paragraph-three">${paragraph3}</p>
+                                            <p class="paragraph-four">${paragraph4}</p>
+                                            </div>
+
+                                            <div class="content-four"> 
+                                            <img src="${img5}" class="image image-five" alt="${headline}"/>
+                                            <p class="paragraph-five">${paragraph5}</p>
+                                            <p class="paragraph-six">${paragraph6}</p>
+                                            </div>
+
+                                            <div class="content-five"> 
+                                            <p class="score"> ${score}</p>
+                                            <p class="lineup-one"> ${lineup1}</p>
+                                            <p class="lineup-two"> ${lineup2}</p>
+                                            </div>
+
+
+                                            </div>
+                                            `
+                                            
+
+                                            // Creating the modal for making the post images bigger
+                                            const images = document.querySelectorAll(".image");
+                                            let imgSrc;
+
+                                            images.forEach((img) => {
+                                                img.addEventListener("click", (e) => {
+                                                    imgSrc = e.target.src;
+                                                    popUpModal(imgSrc);
+                                                    console.log(imgSrc)
+                                                });
+                                            });
+
+                                            let popUpModal = (src) => {
+                                                const modal = document.createElement("div");
+                                                modal.setAttribute("class", "modal");
+                                                document.querySelector(".container").append(modal);
+                                                const newImage = document.createElement("img");
+                                                newImage.setAttribute("src", src);
+                                    
+                                                window.onclick = function (event) {
+                                                    if (event.target == modal) {
+                                                      modal.style.display = "none";
+                                                    }
+                                                  };
+                                    
+                                                modal.append(newImage);
+                                            };
+
+                                            newTitle.innerHTML += `Calcio || ${HTMLTitle}`
+                                            
+                                        } catch (error) {
+                                            contentContainer.innerHTML += `An error has occured, please return to main page or contact us`;
+                                        }
+        }
     
-} 
-fetchContent ();
 
+fetchContent();
 
-function createHTML(content) {
-    const headline = content.acf.h1;
-    const img1 = content.acf.image1;
-    const title = content.acf.h2;
-    const paragraph1 = content.acf.p1;
-    const paragraph2 = content.acf.p2;
-    const img2 = content.acf.image2;
-    const img3 = content.acf.image3;
-    const img4 = content.acf.image4;
-    const paragraph3 = content.acf.p3;
-    const paragraph4 = content.acf.p4;
-    const img5 = content.acf.image5;
-    const paragraph5 = content.acf.p5;
-    const paragraph6 = content.acf.p6;
-    const score = content.acf.score;
-    const lineup1 = content.acf.lineup1;
-    const lineup2 = content.acf.lineup2;
-    const HTMLTitle = content.acf.name;
-
-    contentContainer.innerHTML =    `<div class="blog-content">
-                                    <h1 class="headline">${headline}</h1>
-
-                                    <div class="top">
-                                    <img src="${img1}" id="modalz" class="image-one" alt="${headline}"/>
-                                    </div>
-
-                                    <div class="content-one"> 
-                                    <h2 class="headline-two">${title}</h2>
-                                    <p class="paragraph-one">${paragraph1}</p>
-                                    <p class="paragraph-two">${paragraph2}</p>
-                                    </div>
-
-                                    <div class="content-two"> 
-                                    <img src="${img2}" id="modalz" class="image-two" alt="${headline}"/>
-                                    <img src="${img3}" id="modalz" class="image-three" alt="${headline}"/>
-                                    <img src="${img4}" id="modalz" class="image-four" alt="${headline}"/>
-                                    </div>
-
-                                    <div class="content-three"> 
-                                    <p class="paragraph-three">${paragraph3}</p>
-                                    <p class="paragraph-four">${paragraph4}</p>
-                                    </div>
-
-                                    <div class="content-four"> 
-                                    <img src="${img5}" id="modalz" class="image-five" alt="${headline}"/>
-                                    <p class="paragraph-five">${paragraph5}</p>
-                                    <p class="paragraph-six">${paragraph6}</p>
-                                    </div>
-
-                                    <div class="content-five"> 
-                                    <p class="score"> ${score}</p>
-                                    <p class="lineup-one"> ${lineup1}</p>
-                                    <p class="lineup-two"> ${lineup2}</p>
-                                    </div>
-
-
-                                    </div>
-                                    `
-
-    newTitle.innerHTML += `Calcio || ${HTMLTitle}`;
-
-};
-
-
+   
